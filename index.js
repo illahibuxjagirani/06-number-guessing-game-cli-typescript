76f093ef853142526b78ffcb2662f7733bf34ee4
Your Name <illahibuxjagirani@gmail.com>
2024-06-15 11:04:15 +0500

    first

---
+++ index.js
@@ -0,0 +1,35 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+// our setup is ready we can start now
+console.log("Welcome to Number Guessing Game!\n");
+const secretNumber = Math.floor(Math.random() * 10 + 1);
+let myLoop = true;
+let attempts = 1;
+while (myLoop) {
+    const userInput = await inquirer.prompt({
+        type: 'number',
+        name: 'userNumber',
+        message: "Enter your number!"
+    });
+    let { userNumber } = userInput;
+    console.log(`\nYour attemp No.${attempts}`);
+    if (userNumber === secretNumber)
+        wonMatch();
+    if (userNumber > secretNumber)
+        greaterNum();
+    if (userNumber < secretNumber)
+        lessNum();
+    function wonMatch() {
+        console.log(`Congratulations! Your guessed number ${userNumber} is correct!`);
+        console.log(`\nYou have attempted ${attempts} times to guess the right number!`);
+        myLoop = false;
+    }
+    function greaterNum() {
+        console.log(`Your guessed number is greater than actual number!`);
+        attempts++;
+    }
+    function lessNum() {
+        console.log(`Your guessed number is less than actual number!`);
+        attempts++;
+    }
+}
